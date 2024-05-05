f67aee22b065431f09dd56d8acb096fa260fd07a
Saida-lachgar <s.lachgar@picaxel.com>
2024-05-05 19:12:14 +0100

    pagination server side

--- server/api/controllers/postsController.ts
+++ server/api/controllers/postsController.ts
@@ -1,9 +1,20 @@
 import { Request, Response } from "express";
-import Post from "../models/Post";
+import PostModel from "../models/Post";
+
 
 export const fetchPosts = async (req: Request, res: Response) => {
-  const posts = await Post.find();
-  res.json(posts);
+  const page = parseInt(req.query.page as string) || 1;
+  const limit = parseInt(req.query.limit as string) || 10;
+
+  try {
+    const paginatedResult = await PostModel.paginate({}, { page, limit });
+
+    // Return the paginated result
+    res.json(paginatedResult);
+  } catch (error) {
+    // Handle errors
+    res.status(500).json({ message: 'Error fetching posts', error });
+  }
 };
 
 // export default { fetchPosts }
--- server/api/models/Post.ts
+++ server/api/models/Post.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import paginate from 'mongoose-paginate-v2';
 
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId;
@@ -15,6 +16,27 @@ const PostSchema = new Schema({
   canceled: Boolean,
 });
 
-const PostModel = mongoose.model("Post", PostSchema);
+interface PostData {
+  date?: string;
+  upvotes?: string[];
+  downvotes?: string[];
+  ipAddress?: string;
+  content?: string;
+  hashtags?: string[];
+  reviewed?: boolean;
+  canceled?: boolean;
+}
+
+// paginate with this plugin
+PostSchema.plugin(paginate);
+
+// declare a mongoose document based on a Typescript interface representing your schema
+interface PostDocument extends mongoose.Document, PostData { }
+
+// create the paginated model
+const PostModel = mongoose.model<
+  PostDocument,
+  mongoose.PaginateModel<PostDocument>
+>('Posts', PostSchema, 'posts');
 
 export default PostModel;
